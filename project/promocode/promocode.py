e458df60e55cf4e2ee4e93309f6e50f83b22d4dd
Hiren Chavda <hchavda4122001@gmail.com>
2023-06-06 15:20:46 +0530

    changes

--- project/promocode/promocode.py
+++ project/promocode/promocode.py
@@ -8,9 +8,10 @@ promocode_bp = Blueprint('promocode', __name__)
 def show_promocode():
     cursor = g.db.cursor(dictionary=True)
     cursor.execute(
-        f"SELECT * from tbl_promocode WHERE CURRENT_TIMESTAMP < expire_time;")
+        f"SELECT id,coupon_image,promocode,discription,expire_time,discount_type,discount_value from tbl_promocode WHERE CURRENT_TIMESTAMP < expire_time;")
     coupons = cursor.fetchall()
-    if coupons:        
-        return jsonify(coupons)
+    if coupons:
+        return jsonify({"Show_promocodes": coupons})
+
     else:
         return jsonify({"messege": "Every coupones hase been expired."})
